bb645e0cc74f905e355a571647fcba69076d57f3
kappek <apekshamkalasad@gmail.com>
2014-12-03 06:58:08 +0530

    i dont know what's happening

--- dscSIS.cpp
+++ dscSIS.cpp
@@ -1,87 +1,108 @@
-
+#include<fstream>
 #include<iostream>
 #include<map>
 #include<vector>
 #include<string>
+#include<stdlib.h>
 using namespace std;
 class student
 {
+    static int countAtt;
     string name;
     string usn;
     map<string, int> marks;
     map<string, int> attendance;
 
+
 public:
-    void updateAttendance(int i, char att,string sub);
+    friend void writeToFile(ofstream& of);
+    friend void readFromFile(string USN,ifstream& io);
+    void Attend(string sub,char att);
+    friend void attend(string sub);
 
-    void mark(string sub);
+    student(int x)
+    {
+        cout<<"enter name: ";
+        cin>>name;
+        cout<<"\nenter usn: ";
+        cin>>usn;
 
-    void attend(string sub);
+    }
+    student(){}
+    void display()
+    {
+        cout<<name;
+        cout<<"\n"<<usn;
+    }
 
-    void displayAll();
 
 };
+
+int student::countAtt;
 vector<student> s;
-for(int j=0;j<3;j++)
+vector<student>::iterator it;
+
+
+
+
+void student::Attend(string sub,char att)
 {
-    s.push_back()
+    if(att=='p')
+       attendance.insert(pair<string,int>(sub,++countAtt));
+}
+void writeToFile(ofstream& of)
+{
+    s.push_back(student(1));
+    it=s.end(); --it;
+    of.write((char*)&((*it).name),sizeof((*it).name));
+    of.write((char*)&((*it).usn),sizeof((*it).usn));
+}
+void readFromFile(string USN,ifstream& io)
+{
+
+    student ob;
+     io.read((char*)&((ob).name),sizeof((ob).name));
+    io.read((char*)&((ob).usn),sizeof((ob).usn));
+    if(io.bad())
+        cout<<"bad";
+    cout<<"\nname:"<<ob.name;
+    cout<<"\nusn:"<<ob.usn;
 
 }
-
-
-    void student::updateAttendance(int i, char att, string sub)
-    {
-        map<string, int>::iterator it=s[i].attendance.begin();
-        for(;it!=s[i].attendance.end();++it)
-           {
-              if((it->first)==sub)
-            {
-                if(att='p')
-                    it->second=it->(second++);
-            }
-           }
-    }
-
-     void student::attend(string sub)
-    {
+void attend(string sub)
+{
+        it=s.begin();
         char att;
-        cout<<"\na=absent , p=present\n";
-        for(int i=0;i<s.size();i++)
+        cout<<"a:absent,p:present\n";
+        for(;it!=s.end();it++)
         {
-            cin>>updateAttendance(i,att,sub);
+            cout<<"usn:";
+            cout<<it->usn;
+            cin>>att;
+            it->Attend(sub,att);
         }
-    }
-
-
-     void student::mark(string sub)
-    {
-        map<string, int>::iterator it=s[i].attendance.begin();
-        for(;it!=s[i].attendance.end();++it)
-          {
-              if((it->first)==sub)
-            {
-                cout<<"\nEnter Marks\n";
-                cin>>(it->second);
-            }
-          }
-    }
-   void student::displayAll()
-   {
-
-   }
+}
 
 int main()
 {
-    int ch1,USN,ch2,ch3
-    string sub;
-    cout<<"1.STUDENT LOGIN\n2.TEACHER LOGIN\n";
+
+    int ch1,ch2,ch3,ch4;
+    string sub,USN;
+
+    while(1)
+    { cout<<"1.STUDENT LOGIN\n2.TEACHER LOGIN\n3.Admin\n4.exit\n";
+
     cin>>ch1;
+
+
     switch(ch1)
     {
-        case 1: cout<<"\n Enter USN:\n";
+        case 1: {cout<<"\n Enter USN:\n";
                 cin>>USN;
-                displayAll(USN);
-                break;
+                ifstream io("studentRecords");
+                readFromFile(USN,io);
+                io.close();
+                break;}
         case 2: cout<<"\n1.EC\n2.MATHS\n3.OOPS\n";
                 cin>>ch2;
                 cout<<"\n1.Attendance\n2.Marks\n";
@@ -92,21 +113,39 @@ int main()
                             switch(ch3)
                             {
                                 case 1: attend(sub); break;
-                                case 2: mark(sub); break;
+                                //case 2: mark(sub); break;
                             }
+                            break;
                      case 2: sub="MATHS";
                             switch(ch3)
                             {
                                 case 1: attend(sub); break;
-                                case 2: mark(sub); break;
+                                //case 2: mark(sub); break;
                             }
+                            break;
                      case 3: sub="OOPS";
                             switch(ch3)
                             {
                                 case 1: attend(sub); break;
-                                case 2: mark(sub); break;
+                               // case 2: mark(sub); break;
                             }
+                            break;
                 }
+                break;
+            case 3: cout<<"1.enter\n2.delete\n";
+                    cin>>ch4;
+                    switch(ch4)
+                    {
+                        case 1:{ofstream of("studentRecords");
+                                writeToFile(of);
+                                of.close();
+                                break;}
+                        //case 2: Delete();
+                                //break;
+                    }
+                    break;
+            case 4: exit(0);
+}
     }
     return 0;
 }
